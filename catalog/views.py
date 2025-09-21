22b92d08f6d77445ac667462eba615ea98ca5e9b
shn73 <shn-73@mail.ru>
2025-09-21 17:10:18 +0300

    Change 4

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
 
 
 def home(request):
@@ -9,3 +11,15 @@ def home(request):
 def contacts(request):
     """Контроллер рендерит шаблон страницы контактов функцией"""
     return render(request, "../templates/catalogs/contacts.html")
+
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'product_list.html', context)
+
+
+def product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'product.html', context)
